ae58a297dc0f050a00750d32eddbb910f4b7a0b4
EDWARDPP <eduardoperez53@aragon.unam.mx>
2025-03-11 19:56:57 -0600

    se agregan archivos

---
+++ js/main.js
@@ -0,0 +1,12 @@
+;
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+	$(".dropdown-trigger").dropdown();
+}); 
+document.addEventListener("DOMContentLoaded", function(){
+	$('.preloader-background').delay(1700).fadeOut('slow');
+	
+	$('.preloader-wrapper')
+		.delay(1700)
+		.fadeOut();
+});
